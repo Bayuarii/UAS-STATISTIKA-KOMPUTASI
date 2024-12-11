fea478006bc20b3c3425405104e48b48407b28e4
Putu Bayu Ari Pradnya <barinya0727@gmail.com>
2024-12-11 22:28:58 +0700

    first commit

---
+++ streamlit/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown(
+    "<h1 style='text-align: center;'>PENDETEKSI TINGKATAN STRES MAHASISWA AKHIR</h1>",
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    "Website Ini Akan Membantu Mendeteksi Tingkat Stres Yang Kalian Miliki &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:"
+)
+
+
